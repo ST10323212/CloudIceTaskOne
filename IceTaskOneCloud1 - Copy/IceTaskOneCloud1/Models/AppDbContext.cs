0be7ff85a0c0050a481ed9c8eff3ed1d8f6673ba
ST10323212 <ST10323212@vcconnect.edu.za>
2024-03-22 19:14:00 +0200

    CloudIceTaskOne
    my task for ice task one

---
+++ IceTaskOneCloud1 - Copy/IceTaskOneCloud1/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IceTaskOneCloud1.Models
+{
+    using System.Data.Entity;
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<DivingEquipment> DivingEquipments { get; set; }
+    }
+
+}
